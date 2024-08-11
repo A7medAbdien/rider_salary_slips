501611b2c6d7c8aae635a107b40b02360bb9d849
A7medAbdien <ahmed.g.abdien.com>
2024-08-11 12:44:49 +0300

    update the format

--- rider_salary_slips/rider_salary_slips/doctype/rider_salary_slip/rider_salary_slip.py
+++ rider_salary_slips/rider_salary_slips/doctype/rider_salary_slip/rider_salary_slip.py
@@ -2,22 +2,30 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+# from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 from datetime import datetime
 from frappe.utils import add_to_date
 
 
-class RiderSalarySlip(Document):
+class RiderSalarySlip(WebsiteGenerator):
     def validate(self):
         if not self.start_date:
             today = datetime.today()
-            first_day_of_month = datetime(today.year, today.month, 1)
-            self.start_date = first_day_of_month.strftime("%Y-%m-%d")
+            first_day_of_previous_month = (
+                datetime(today.year, today.month - 1, 1)
+                if today.month > 1
+                else datetime(today.year - 1, 12, 1)
+            )
+            self.start_date = first_day_of_previous_month.strftime("%Y-%m-%d")
+
         if not self.end_date:
-            today = datetime.today()
-            first_day_of_next_month = datetime(today.year, today.month + 1, 1)
-            last_day_of_month = add_to_date(first_day_of_next_month, days=-1)
-            self.end_date = last_day_of_month
+            first_day_of_current_month = datetime(today.year, today.month, 1)
+            last_day_of_previous_month = add_to_date(
+                first_day_of_current_month, days=-1
+            )
+            self.end_date = last_day_of_previous_month.strftime("%Y-%m-%d")
+
         if not self.payroll_month:
             date = datetime.strptime(self.start_date, "%Y-%m-%d")
             self.payroll_month = date.strftime("%B")
