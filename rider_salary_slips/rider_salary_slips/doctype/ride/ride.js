88258530ea736cfe79912a29824a9322ac25ef07
A7medAbdien <ahmed.g.abdien.com>
2024-08-12 15:02:11 +0300

    update riders doc type name

--- rider_salary_slips/rider_salary_slips/doctype/ride/ride.js
+++ rider_salary_slips/rider_salary_slips/doctype/ride/ride.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, ahmed.g.abdin@gmail.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Riders", {
+// frappe.ui.form.on("Ride", {
 // 	refresh(frm) {
 
 // 	},
--- rider_salary_slips/rider_salary_slips/doctype/ride/ride.py
+++ rider_salary_slips/rider_salary_slips/doctype/ride/ride.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Riders(Document):
+class Ride(Document):
 	pass
